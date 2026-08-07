18d9eb21ad414566f16493319c875c13331f372a
an-core <66536532+an-core@users.noreply.github.com>
2026-08-07 22:12:41 +0300

    Add files via upload

--- images/store/functions.js
+++ images/store/functions.js
@@ -2031,7 +2031,7 @@
           showToast('Товар "' + product.name + '"' + colorName + ' добавлен в корзину!');
       });
 
-      const GITHUB_BASE_URL = 'https://an-core.github.io/f-mage/';
+      const GITHUB_BASE_URL = 'https://an-core.github.io/f_mage/';
 
       async function loadAllImages() {
           const catalog = document.getElementById('catalogContainer');
